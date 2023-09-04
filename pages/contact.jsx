6d6875746354c3ed3b1cfcc8953000f266925204
Val Sab <valerie.sabando@gmail.com>
2023-09-04 15:25:49 +0200

    Personalizs information such as name and contacts

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -17,12 +17,12 @@ function Contact() {
               Contact
             </h1>
             <p className="description">
-              Incididunt in cillum magna occaecat nisi qui in exercitation
-              labore anim adipisicing amet irure tempor.
+              If you have any questions or interested in a colloboration, feel
+              free to contact me.
             </p>
             <ul className="contact-links">
-              <li className="contact-item">Email: lutfhi@me.com</li>
-              <li className="contact-item">Phone: +6212344321007</li>
+              <li className="contact-item">Email: val.sab@gmail.com</li>
+              <li className="contact-item">Phone: +43 677 12345678</li>
             </ul>
           </div>
         </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -15,15 +15,15 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hello, I'm Valerie
             </h1>
             <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
-              antarmuka pengguna, dan penulis.
+              I'm working in customer service and aiming on becoming a web
+              developer.
             </p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              My contact details
             </Link>
           </div>
 
